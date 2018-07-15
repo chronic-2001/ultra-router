c1a5e6c5b4cf2fc0b097f6c44b3b37f1dd115b75
Chronic Wang <chronic.wang@blackboard.com>
2018-07-15 20:29:50 +0800

    Minor refine.

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,7 @@
 import { Request, Response, RequestHandler } from 'express';
 import { ClientRequest, IncomingMessage, ServerResponse, RequestOptions, request as http } from 'http';
 import { request as https } from 'https';
-import { isFunction, pick, clone, forEach, noop } from 'lodash';
+import { pick, clone, forEach, noop } from 'lodash';
 import { parse } from 'url';
 
 interface IOptions {
@@ -9,7 +9,7 @@ interface IOptions {
   timeout?: number; // timeout for this proxy in ms
   filters?: RequestHandler[];
   onRequest?: (proxyReq: ClientRequest, req: IncomingMessage) => void;
-  onResponse?: (proxyRes: IncomingMessage, res: ServerResponse) => void;
+  onResponse?: (proxyRes: IncomingMessage, res: ServerResponse) => void | Promise<void>;
 }
 
 export default function proxy({ target, timeout, filters = [], onRequest = noop, onResponse = noop }: IOptions) {
