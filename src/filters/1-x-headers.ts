86b3a379bb0148f82f4ea5150ae5e0f60f971f05
Chronic Wang <chronic.wang@blackboard.com>
2018-08-01 10:21:53 +0800

    Remove x-forwarded-proto header.

--- src/filters/1-x-headers.ts
+++ src/filters/1-x-headers.ts
@@ -4,7 +4,10 @@ import * as crypto from 'crypto';
 export = async function setXHeaders(req: Request, res: Response, next: NextFunction) {
   const config = await import('../config');
   const headers = req.headers;
-  headers['x-forwarded-proto'] = config.protocol;
+  // The x-forwarded-proto header will be used by tomcat RemoteIpValve to change the protocol and port of the original request,
+  // this is undesirable for this router, so do not set this header.
+  // headers['x-forwarded-proto'] = config.protocol;
+
   // strip the ::ffff: prefix of the converted ipv6 address, e.g. ::ffff:127.0.0.1
   headers['x-forwarded-for'] = headers['x-real-ip'] = req.socket.remoteAddress.replace(/^.*:/, '');
   headers['x-forwarded-host'] = headers['host'] = headers['host'] || config.hostname + ':' + config.port;
